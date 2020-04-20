e3edf0a615b0330f9b1f5a33b21bf5333a2dd5ff
tunykun <anthonyargel00@gmail.com>
2020-04-19 20:09:25 -0700

    Update dlsite_example.py

--- dlsite_example.py
+++ dlsite_example.py
@@ -5,7 +5,7 @@ from dlsite_scraper import dlsite_scraper
 
 crawler = dlsite_scraper()
 url = 'https://www.dlsite.com/maniax/circle/profile/=/language/jp/sex_category%5B0%5D/male/keyword_maker_name/RG24350/order%5B0%5D/release_d/per_page/100/hd/1/page/1#works'
-filename = 'momoirotest' # The path where the file will be saved. Here it will be saved to data/momoirotest.json
+filename = 'momoirotest' # The path where the file will be saved. Here it will be saved to momoirotest.json
 save_type = 'json' # can save as csv or json
 crawler.save_multiple_from_url(url, name_of_file = filename, filetype_passed = save_type)
 
